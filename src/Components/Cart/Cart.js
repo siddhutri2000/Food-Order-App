d0abcb3a9ff5acfe8ba4bff423dd9d423c13fb19
sid <siddhu.tri2000@gmail.com>
2022-01-30 11:25:34 +0530

    removing items from the cart

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -10,7 +10,9 @@ const Cart = (props) => {
 
   const hasItems = cartCtx.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {};
+  const cartItemRemoveHandler = (id) => {
+    cartCtx.removeItem(id);
+  };
 
   const cartItemAddHandler = (item) => {
     cartCtx.addItem(item);
--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,5 +1,6 @@
 import CartContext from "./cart-context";
 import { useReducer } from "react";
+import { act } from "react-dom/cjs/react-dom-test-utils.production.min";
 
 const defaultCartState = {
   items: [],
@@ -9,9 +10,45 @@ const defaultCartState = {
 const cartReducer = (state, action) => {
   //we made this function here because we dont want it to be reevalutaed when the component reloads.
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+    let updatedItems;
+
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === "REMOVE") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemIndex] = updatedItem;
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
